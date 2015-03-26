8273e660b8b685e939c3f4014f0d3ce7efe46b8a
Quarx2k <agent00791@gmail.com>
2015-03-27 00:22:14 +0600

    Replace camera2 with asus camera

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -64,7 +64,8 @@ PRODUCT_PACKAGES += \\
     com.qualcomm.location \\
     QuickBoot \\
     qcrilmsgtunnel \\
-    shutdownlistener
+    shutdownlistener \\
+    AsusCamera
 
 PRODUCT_PACKAGES += \\
     libtime_genoff \\
@@ -196,6 +197,17 @@ LOCAL_MODULE_PATH := \$(TARGET_OUT_VENDOR_SHARED_LIBRARIES)
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_SUFFIX := .so
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+include \$(BUILD_PREBUILT)
+
+include \$(CLEAR_VARS)
+LOCAL_MODULE := AsusСamera
+LOCAL_OVERRIDES_PACKAGES := Camera2
+LOCAL_MODULE_OWNER := $VENDOR
+LOCAL_SRC_FILES := proprietary/app/AsusCamera/AsusCamera.apk
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_SUFFIX := \$(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_MODULE_CLASS := APPS
+LOCAL_CERTIFICATE := platform
 include \$(BUILD_PREBUILT)
 
 endif

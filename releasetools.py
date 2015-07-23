a4d44987f30eefc57c12cf4a753dac10630bf3f0
Quarx2k <agent00791@gmail.com>
2015-07-23 18:07:16 +0600

    Add modem,rpm,asusfw,tz flash with CM

--- releasetools.py
+++ releasetools.py
@@ -24,3 +24,19 @@ def FullOTA_InstallEnd(self):
   self.script.Mount("/system")
   self.script.AppendExtra('run_program("/tmp/fixup.sh");')
   self.script.Unmount("/system");
+
+  self.output_zip.write(os.path.join(TARGET_DIR, "firmware-update/tz.mbn"), "firmware-update/tz.mbn")
+  self.output_zip.write(os.path.join(TARGET_DIR, "firmware-update/NON-HLOS.bin"), "firmware-update/NON-HLOS.bin")
+  self.output_zip.write(os.path.join(TARGET_DIR, "firmware-update/asusfw.img"), "firmware-update/asusfw.img")
+  self.output_zip.write(os.path.join(TARGET_DIR, "firmware-update/rpm.mbn"), "firmware-update/rpm.mbn")
+  self.script.AppendExtra('ui_print("installing tz");')
+  self.script.AppendExtra('package_extract_s_file("firmware-update/tz.mbn", "/dev/block/platform/msm_sdcc.1/by-name/tz");')
+  self.script.AppendExtra('ui_print("installing modem");')
+  self.script.AppendExtra('package_extract_s_file("firmware-update/NON-HLOS.bin", "/dev/block/platform/msm_sdcc.1/by-name/modem");')
+  self.script.AppendExtra('ui_print("installing asusfw");')
+  self.script.AppendExtra('package_extract_s_file("firmware-update/asusfw.img", "/dev/block/platform/msm_sdcc.1/by-name/asusfw");')
+  self.script.AppendExtra('ui_print("installing rpm");')
+  self.script.AppendExtra('package_extract_s_file("firmware-update/rpm.mbn", "/dev/block/platform/msm_sdcc.1/by-name/rpm");')
+
+
+

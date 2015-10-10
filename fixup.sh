454bee922129188926f44ea789b951de1a40e27a
Quarx2k <agent00791@gmail.com>
2015-10-10 18:15:35 +0600

    Fix detect X or S

--- fixup.sh
+++ fixup.sh
@@ -1,17 +1,18 @@
 #!/sbin/sh
 
 model=`cat /proc/cmdline | sed 's/.*HW_ID=\([^ ]*\).*/\1/'`
-PF500KL="PF500KL"
-A91="A91"
+PF500KL='PF500KL'
+A91='A91'
 
 mkdir /asusfw
 mount -t ext4 /dev/block/platform/msm_sdcc.1/by-name/asusfw /asusfw
 
-if [[ "$model" == *"$A91"* ]]; then
+if [[ ! -z $(echo ${model} | grep ${A91}) ]]; then
+ echo  "This is PadFone X"
  cp -R /asusfw/nfc/libpn544_c3_fw.so  /system/vendor/firmware/libpn544_fw.so
-else if [[ "$model" == *"$PF500KL"* ]]; then
+elif [[ ! -z $(echo ${model} | grep ${PF500KL}) ]]; then
+ echo  "This is PadFone S"
  cp -R /asusfw/nfc/libpn544_c2_fw.so  /system/vendor/firmware/libpn544_fw.so
-fi
 fi
 
 umount /asusfw

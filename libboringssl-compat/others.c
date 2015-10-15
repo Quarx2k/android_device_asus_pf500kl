2896911c7e7ef69829c2f8a61c917690d69ee98d
Quarx2k <agent00791@gmail.com>
2015-10-16 00:01:49 +0600

    Update QMI to 6.0 MRA58K, add stubs for old sensors, use aosp ril

--- libboringssl-compat/others.c
+++ libboringssl-compat/others.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015 The Android Open Source Project
+ * Copyright (C) 2015 The Android Open Source Project, Quarx2k(https://github.com/Quarx2k)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-
+// Stubs for libOpenCL.so
 int cb_event_notify_egl_sync_is_signaled_qcom() {
     return 0;
 }
@@ -23,4 +23,11 @@ int cb_event_register_egl_sync_observer_qcom() {
     return 0;
 }
 
+// Stubs for old sensors.qcom with new QMI
+int qmi_client_init_instance() {
+   return 0;
+}
 
+int qmi_client_register_notify_cb() {
+   return 0;
+}

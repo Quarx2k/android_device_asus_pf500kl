b8fb8355eec58de6f2506b8e91fd1ff925dc5f0d
Quarx2k <agent00791@gmail.com>
2016-02-06 17:26:10 +0600

    Remove fm config script

--- t00n.mk
+++ t00n.mk
@@ -264,8 +264,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/init.asus.thermal_conf.sh:system/etc/init.asus.thermal_conf.sh \
     $(LOCAL_PATH)/configs/init.qcom.bt.sh:system/etc/init.qcom.bt.sh \
-    $(LOCAL_PATH)/configs/onPadEventChanged.sh:system/etc/onPadEventChanged.sh \
-    $(LOCAL_PATH)/configs/init.qcom.fm.sh:system/etc/init.qcom.fm.sh
+    $(LOCAL_PATH)/configs/onPadEventChanged.sh:system/etc/onPadEventChanged.sh
 
 # Add own hdmi mode: EDIDData(HDMI_VFRMT_1920x1200p60_16_10, 1920, 1200, 60, 25),
 PRODUCT_COPY_FILES += \
